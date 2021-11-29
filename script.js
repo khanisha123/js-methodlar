e87791500ef8abbb49bec8f97052995c6ebe993d
khanisha123 <khanisha@code.edu.az>
2021-11-29 20:41:51 +0400

    Method yazmaq

---
+++ script.js
@@ -0,0 +1,79 @@
+// First task task 1 (1.1)
+
+// let strReverse=str=>str.split('').reverse().join('');
+// console.log(strReverse('hello everyone'));
+
+
+// Secound task 1 (2.1) map
+// function myFunction(arr) {
+//    for (let i = 0;i< arr.length; i++) {
+//       arr[i]*=2
+//    }
+//     return arr;
+// }
+// console.log(myFunction([1,2,3,8,5]));
+
+
+// Secound task 2 (2.2) reduce
+// let sum=0;
+// function myFunction(arr) {
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         sum+=arr[i]
+//     }
+//     return sum;
+// }
+// console.log(myFunction([1,2,3,8,5]));
+
+// Secound task 3 (2.3) filter
+    // let arr=[11,22,43,14,85,56,117,68];
+    // arr.forEach(num => {
+    //     if (num>20) {
+    //         console.log(num);
+    //     }
+    // });
+    
+
+// Secound task 4 (2.4) every
+// function myFunction(arr) { 
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (arr[i]>18) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+// console.log(myFunction([45,19,96,55,43,22]));
+
+// Secound task 5 (2.5) find
+// let arr=[45,19,96,55,43,22]
+//  for (let index = 0; index < arr.length; index++) {
+//      const element = arr[index];
+//      if (element>20) {
+//          console.log(element);
+//          break
+//      }else{
+//          console.log("underfind");
+//      }
+     
+//  }
+
+// Secound task 6 (2.6) foreach
+// let arr=[1,2,3,4,5,6,7,8]
+// let count={};
+// arr.forEach(item => {
+//     if (count[item]) {
+//         count[item]++;
+
+//     }else{
+//         count[item]=1
+//     }
+// });
+
+// console.log(count);
+
+
+
